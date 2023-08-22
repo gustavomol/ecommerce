58c26e5de989dc1da6a9e3e75195baf14dee4b60
Gustavo Molina <igustavo.mol@gamil.com>
2023-08-22 01:21:12 -0600

    package.json updated after MUI instalation, using MUI Button on NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import CarWidget from "../CartWidget/CartWidget"
+import Button from '@mui/material/Button';
 
 const NavBar = () => {
     return(
         <nav>
             <h2>Instrumentos Musicales</h2>
             <div>
-                <button>Guitarras</button>
-                <button>Bajos</button>
-                <button>Accesorios</button>
+                <Button variant="outlined">Guitarras</Button>
+                <Button variant="outlined">Bajos</Button>
+                <Button variant="outlined">Accesorios</Button>
                 <CarWidget />
             </div>
         </nav>
