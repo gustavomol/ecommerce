7fc00b160860826cee517c12f3da9e2d9cdc3d06
Gustavo Molina <igustavo.mol@gamil.com>
2023-09-11 16:44:16 -0600

    react-router-dom was instaled so package.json updated, App uses routes now, NavBar uses Link, new Nosotros component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './main.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
+import Nosotros from './components/Nosotros/Nosotros';
+import './main.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <ItemListContainer greeting={'Bienvenidos'}/>
-      <ItemCount initial={1} stock={5} onAdd={(qty) => console.log('Agregando: ', qty)} />
-    </div>
+
+      <BrowserRouter>
+      
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting={'Bienvenidos'}/>} />
+          <Route path="cart" element={<ItemCount initial={1} stock={5} onAdd={(qty) => console.log('Agregando: ', qty)} />} />
+          <Route path="nosotros" element={<Nosotros />} />
+        </Routes>
+
+      </BrowserRouter>
+
+      // <ItemCount initial={1} stock={5} onAdd={(qty) => console.log('Agregando: ', qty)} />
+
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,18 @@
 import CarWidget from '../CartWidget/CartWidget'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = () => {
     return(
         <nav className='navbar'>
-            <a href="#" className='logo'><h2>Mi Tienda</h2></a>
+            <Link to="#" className='logo'><h2>Mi Tienda</h2></Link>
      
             <div>
                 <ul className='menu'>
-                    <li><a className='menu-link' href='#Inicio'>Inicio</a></li>
-                    <li><a className='menu-link' href='#Inicio'>Productos</a></li>
-                    <li><a className='menu-link' href='#Inicio'>Nosotros</a></li>
-                    <li><a className='menu-link' href='#Inicio'>Contacto</a></li>
+                    <li><Link className='menu-link' to='/'>Inicio</Link></li>
+                    <li><Link className='menu-link' to='/'>Productos</Link></li>
+                    <li><Link className='menu-link' to='/nosotros'>Nosotros</Link></li>
+                    <li><Link className='menu-link' to='/contacto'>Contacto</Link></li>
                 </ul>
             </div>
             <CarWidget />
